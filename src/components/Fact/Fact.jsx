ebc4ded7cd8fde773774a0f7cffa02c9b5342503
Detective Pikachu <frenzyexists@gmail.com>
2022-03-22 01:15:59 -0400

    Cleaned unused files, CSS is done, minimum project requirements filled

--- src/components/Fact/Fact.jsx
+++ src/components/Fact/Fact.jsx
@@ -1,24 +1,27 @@
+import './Fact.css';
+import s1 from './s1.svg'
+import s2 from './s2.svg'
+import s3 from './s3.svg'
+import s4 from './s4.svg'
+import s5 from './s5.svg'
+let topic = "Pokemon" // Testing how variables work here
 
-let topic = "Pokemon"
 
 const Facts = () => {
     return (
-        <div>
-            <h1>
+        <div className="fact-container">
+            <h1 id="topic">
                 Fun Facts about... {topic}
             </h1>
             <div>
-
-                {/* Remove these */}
-                <ul>
-                    <li>Ivysaur is the only Generation 1 Pokémon whose name begins with the letter I</li>
-                    <li>Meowth is the first Pokémon in the animé to talk</li>
-                    <li>Abra has the highest Special Attack of all unevolved Pokémon</li>
-                    <li>Gastly and its evolution Haunter are the lightest Pokémon in existence</li>
-                    <li>Scyther hates the color red, according to the animé episode Showdown in Dark City</li>
-                    <li>Krabby can grow back its pincers in a short amount of time if they are lost during battle.</li>
+                <ul className="fact-list">
+                    <li><img src={s1} alt="ø"/>Ivysaur is the only Generation 1 Pokémon whose name begins with the letter I</li>
+                    <li><img src={s2} alt="ø"/>Meowth is the first Pokémon in the animé to talk</li>
+                    <li><img src={s3} alt="ø"/>Abra has highest Special Attack of all unevolved Pokémon</li>
+                    <li><img src={s4} alt="ø"/>Gastly and its evolution Haunter are the lightest Pokémon in existence</li>
+                    <li><img src={s5} alt="ø"/>Scyther hates the color red, according to the animé episode Showdown in Dark City</li>
+                    <li><img src={s1} alt="ø"/>Krabby can grow back its pincers in a short amount of time if they are lost during battle.</li>
                 </ul>
-
             </div>
         </div>
     );
--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
       <App />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-      <link href="https://fonts.googleapis.com/css2?family=Inria+Sans:wght@400;700&family=Itim&family=Montserrat+Alternates&display=swap" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css2?family=Mali:ital,wght@0,400;0,600;1,400;1,600&family=Manjari:wght@400;700&display=swap" rel="stylesheet"/> 
   </React.StrictMode>,
   document.getElementById('root')
 );
